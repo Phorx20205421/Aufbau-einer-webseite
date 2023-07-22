85c74f39c7cbd2e9acb3d1963f1db732ac4eda39
Phorx20205421 <phorxgamer@gmail.com>
2023-07-23 01:05:21 +0200

    Testwebseite

---
+++ script.js
@@ -0,0 +1,5 @@
+// Funktion zum Ein- und Ausblenden des Menüs
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+}
